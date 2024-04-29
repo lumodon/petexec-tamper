06ca47a7382edd269fe37d91015af728af72dcfc
Liv <10302608+lumodon@users.noreply.github.com>
2024-04-29 10:39:04 -0700

    Initialization

---
+++ main.js
@@ -0,0 +1,193 @@
+(function() {
+  'use strict';
+  const DEBUG_MODE = false;
+  const outerContainer = document.querySelector('body > nav')?.nextElementSibling;
+  const mainContentDiv = outerContainer.querySelector('.sub-page');
+  console.log('mainContent: ', mainContentDiv);
+  const runOnPetSearch = () => {
+    const observer = new MutationObserver((mutationList, _observer) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === "childList" && mutation.addedNodes.length) {
+          (async () => {
+            console.log('changed, checking');
+            for (const itNode of Array.from(mutation.addedNodes)) {
+              if (itNode.nodeName === 'H1') {
+                const found = await checkAndRun(itNode);
+                if (found) {
+                  console.log('Found and done updating');
+                  return;
+                }
+              }
+            }
+            console.log('no Header found so abandoning');
+          })();
+        //} else if (mutation.type === "attributes") {
+        //  console.log(`The ${mutation.attributeName} attribute was modified.`);
+        }
+      }
+    });
+
+    observer.observe(outerContainer, { attributes: false, childList: true, subtree: false });
+    observer.observe(mainContentDiv, { attributes: false, childList: true, subtree: false });
+  };
+
+  const restyleButtons = () => {
+    const eTable = document.querySelector('#top_search_table');
+    const eTBody = eTable.querySelector('tbody');
+
+    Array.from(document.querySelectorAll('.lumo-signin')).forEach(qbtn => {
+      const rowId = qbtn.getAttribute('data-orig-order');
+      const belongingRow = eTBody.querySelector(`[data-orig-order="${rowId}"`);
+      console.log(belongingRow.offsetTop, belongingRow, qbtn);
+      qbtn.style.top = `${belongingRow.offsetTop+4}px`;
+      qbtn.style.height = `${belongingRow.clientHeight-12}px`;
+    });
+  };
+
+  const checkAndRun = (indicatingElement) => new Promise((resolve, _reject) => {
+    const correctPage = indicatingElement?.innerText.includes('Pet Search');
+    if (!correctPage) {
+      console.log('Not Pet Search page. Exiting');
+      resolve(false);
+      return;
+    }
+
+    resolve(true);
+
+    const eTable = document.querySelector('#top_search_table');
+    const eTBody = eTable.querySelector('tbody');
+
+    if (!eTBody) return;
+
+    const eTableWrapper = document.querySelector('#top_search_table_wrapper');
+    const eSortingClicks = Array.from(eTable.querySelectorAll('thead .sorting'));
+    const ePlayArea = eSortingClicks[eSortingClicks.length - 1];
+
+    ePlayArea.innerText = 'Quick Signin';
+    if (!eTable || !eTableWrapper || !eTBody) {
+      console.warn('top_search_table or derivatives not found. Aborting');
+      return;
+    }
+
+    Array.from(eTBody.children).forEach((it, index) => {
+      it.setAttribute('data-orig-order', String(index));
+      const eText = it.querySelector('a span:nth-of-type(2)');
+      if (DEBUG_MODE) {
+        eText.innerText = `${index} ${eText.innerText}`;
+      }
+    });
+    eSortingClicks.forEach(it => {
+      it.addEventListener('click', () => {
+        setTimeout(() => {
+          restyleButtons();
+        }, 100);
+      });
+    });
+
+    const eRows = [
+      ...Array.from(eTable.querySelectorAll('tr[role="row"].odd')).filter(it => it.querySelector('td')?.innerHTML?.length > 0),
+      ...Array.from(eTable.querySelectorAll('tr[role="row"].even')).filter(it => it.querySelector('td')?.innerHTML?.length > 0),
+    ];
+
+    eRows.forEach(eRowIt => {
+      const photoElement = eRowIt.querySelector('td:nth-of-type(6) a');
+      if (!photoElement) return;
+      const photoElementParent = photoElement.parentNode;
+      const photoData = photoElement.dataset.content;
+      if (!photoData?.length) return;
+      photoElement.outerHTML = photoElement.dataset.content;
+      const imgElement = photoElementParent.querySelector('img');
+      imgElement.addEventListener('load', () => {
+        restyleButtons();
+      });
+    });
+
+    eRows.forEach(eRowIt => {
+      const signInBtn = Array.from(eRowIt.querySelectorAll('.dropdown-menu a[role="menuitem"]')).filter(it => it.innerText === 'Sign into Daycare')?.[0];
+      const schedBoardingBtn = Array.from(eRowIt.querySelectorAll('.dropdown-menu a[role="menuitem"]')).filter(it => it.innerText === 'Schedule a Boarding')?.[0];
+
+      const rects = eRowIt.getBoundingClientRect();
+
+      const newLnkSignin = signInBtn.cloneNode();
+      const newLnkBoarding = schedBoardingBtn.cloneNode();
+      const rowId = Number(eRowIt.getAttribute('data-orig-order'));
+      const dogName = eRowIt.querySelector('td div a#dropdownMenu2')?.innerText?.trim?.()?.split(' ')?.[0]?.slice(0,16);
+      const lastName = eRowIt.querySelector('td:nth-of-type(2) div')?.innerText?.trim?.()?.split(',')?.[0]?.slice?.(0,1);
+
+      newLnkSignin.innerText = `${DEBUG_MODE ? `${rowId} ` : ''}${dogName} ${lastName}`;
+      newLnkBoarding.innerText = 'Quick boarding';
+      const newStyle = {
+        position: 'absolute',
+        left: `${ePlayArea.offsetLeft}px`,
+        top: `${eRowIt.offsetTop+4}px`,
+        width: `${ePlayArea.clientWidth - 9}px`,
+        margin: '3px 6px',
+        height: `${rects.height-12}px`,
+        backgroundImage: `linear-gradient(to right bottom, #fdbfe2, #f9c2e5, #f5c4e7, #f1c7e9, #edc9eb, #ecceee, #ebd4f1, #ebd9f3, #eee3f7, #f2edfb, #f8f6fd, #ffffff)`,
+        boxShadow: `2px 2px 8px 0px rgba(148,107,148,1)`,
+        textAlign: 'center',
+        color: '#537bd5',
+        fontWeight: 'bolder',
+        fontSize: '15px',
+        textWrap: 'nowrap',
+        borderRadius: '15px',
+        padding: '5px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      };
+      const boardingStyle = {
+        ...newStyle,
+        backgroundColor: '#ccf',
+      };
+      Object.keys(newStyle).forEach(key => {
+        newLnkSignin.style[key] = newStyle[key];
+      });
+
+      newLnkSignin.setAttribute('data-orig-order', String(rowId));
+      newLnkSignin.classList.add('lumo-signin');
+      newLnkSignin.addEventListener('click', () => {
+        window.localStorage.setItem('quickLinkUsed', 'true');
+      });
+
+      eTableWrapper.appendChild(newLnkSignin);
+    });
+  });
+
+  if (window.location.href.includes('https://secure.petexec.net/admin/receipt.php?orderId=')) {
+    const isPaid = document.evaluate(`//h3[text()="Account Balance"]`, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null)
+      ?.singleNodeValue?.nextSibling?.data?.includes?.('this account is in balance');
+    const parentSection = document.querySelector('#mainContentDiv.sub-page');
+    const newInfoBlock = document.createElement('div');
+    const newStyle = {
+      margin: '15px auto',
+      width: '140px',
+      height: '140px',
+      fontWeight: 'bolder',
+      backgroundColor: isPaid ? '#afa' : '#f99',
+      textAlign: 'center',
+      fontSize: '32px',
+      padding: '23px 5px',
+      borderRadius: '15px',
+    };
+    newInfoBlock.innerText = isPaid ? 'Paid In Full' : 'NOT PAID!';
+    Object.keys(newStyle).forEach(key => {
+      newInfoBlock.style[key] = newStyle[key];
+    });
+    parentSection.insertBefore(newInfoBlock, parentSection.children[0]);
+    return;
+  }
+
+  if (window.location.href.includes('https://secure.petexec.net/admin/daycareSignin2.php?uid=')) {
+
+    const eActionBtn = document.querySelector('#theAction');
+
+    const quickLinkUsed = window.localStorage.getItem('quickLinkUsed');
+    if (quickLinkUsed === 'true') {
+      window.localStorage.setItem('quickLinkUsed', 'false');
+      eActionBtn.click();
+    }
+  }
+
+  runOnPetSearch();
+})();
---
+++ script.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         PetExec Quick Signin
+// @namespace    http://tampermonkey.net/
+// @version      2024-04-12
+// @description  Makes PetExec easier to live with
+// @author       Lumi
+// @match        https://secure.petexec.net/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=petexec.net
+// @grant        none
+// @run-at       document-idle
+// @require      waiting for first push
+// ==/UserScript==
